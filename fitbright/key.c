38958a0c2e29a7f38aaa9c64a1372f6d274365ce
hanyz <championhan@163.com>
2015-08-31 15:29:28 +0800

    更改按键IO口

--- fitbright/key.c
+++ fitbright/key.c
@@ -1,44 +1,152 @@
 #include "key.h"
 #include "stm32f10x_gpio.h"
 
-#define KEY_PIN_0      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_0) 
-#define KEY_PIN_1      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1)
-#define KEY_PIN_2      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_2)
-#define KEY_PIN_3      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_3)
-#define KEY_PIN_4      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_4)
-#define KEY_PIN_5      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_5)
-#define KEY_PIN_6      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_6)
-#define KEY_PIN_7      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_7)
-#define KEY_PIN_8      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_8)
-#define KEY_PIN_9      GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_9)
-#define KEY_PIN_U      GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_0)
-#define KEY_PIN_D      GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_1)
-#define KEY_PIN_L      GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_2)
-#define KEY_PIN_R      GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_3)
-#define KEY_PIN_M      GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_5)
-#define KEY_PIN_UD     GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_11)
-#define KEY_PIN_C      GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_10)
-#define KEY_PIN_DE     GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_9)
-#define KEY_PIN_OK     GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_8)
+#define GPIO_KEY_0     GPIOA
+#define Pin_Key_0      GPIO_Pin_4
+
+#define GPIO_KEY_1     GPIOA
+#define Pin_Key_1      GPIO_Pin_1
+
+#define GPIO_KEY_2     GPIOA
+#define Pin_Key_2      GPIO_Pin_0
+
+#define GPIO_KEY_3     GPIOC
+#define Pin_Key_3      GPIO_Pin_3
+
+#define GPIO_KEY_4     GPIOC
+#define Pin_Key_4      GPIO_Pin_2
+
+#define GPIO_KEY_5     GPIOC
+#define Pin_Key_5      GPIO_Pin_1
+
+#define GPIO_KEY_6     GPIOA
+#define Pin_Key_6      GPIO_Pin_5
+
+#define GPIO_KEY_7     GPIOA
+#define Pin_Key_7      GPIO_Pin_6
+
+#define GPIO_KEY_8     GPIOA
+#define Pin_Key_8      GPIO_Pin_7
+
+#define GPIO_KEY_9     GPIOC
+#define Pin_Key_9      GPIO_Pin_4
+
+#define GPIO_KEY_UP    GPIOC
+#define Pin_Key_UP     GPIO_Pin_5            //向上键
+
+#define GPIO_KEY_DOWN  GPIOB
+#define Pin_Key_DOWN   GPIO_Pin_0            //向下键
+
+#define GPIO_KEY_LF    GPIOB
+#define Pin_Key_LF     GPIO_Pin_1            //向左键
+
+#define GPIO_KEY_RT    GPIOB
+#define Pin_Key_RT     GPIO_Pin_2            //向右键
+
+#define GPIO_KEY_MODE  GPIOB
+#define Pin_Key_MODE   GPIO_Pin_13           //模式键
+
+#define GPIO_KEY_MENU  GPIOB
+#define Pin_Key_MENU   GPIO_Pin_14           //功能键
+
+#define GPIO_KEY_INPT  GPIOB
+#define Pin_Key_INPT   GPIO_Pin_15           //输入切换键 
+
+#define GPIO_KEY_DEL   GPIOF                         
+#define Pin_Key_DEL    GPIO_Pin_7            //删除键
+
+#define GPIO_KEY_OK     GPIOF
+#define Pin_Key_OK      GPIO_Pin_6           //确定键
+
+
+typedef enum{
+	
+	
+	
+	
+};
 
 void key_init(void)
 {
   GPIO_InitTypeDef GPIO_InitStructure; 
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;  
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_0;  
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_Init(GPIO_KEY_0, &GPIO_InitStructure);
 	
 	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable,ENABLE);//PB3用作普通IO
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 |
-	                              GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;  
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_1;  
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 |
-	                              GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;  
+	GPIO_Init(GPIO_KEY_1, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_2;  
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
-	GPIO_Init(GPIOC, &GPIO_InitStructure);	   
+	GPIO_Init(GPIO_KEY_2, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_3;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_3, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_4;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_4, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_5;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_5, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_6;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_6, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_7;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_7, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_8;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_8, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_9;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_9, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_UP;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_UP, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_DOWN;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_DOWN, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_MODE;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_MODE, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_MENU;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_MENU, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_DEL;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_DEL, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_OK;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_OK, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_LF;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_LF, &GPIO_InitStructure);
+		
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_RT;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_RT, &GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Pin = Pin_Key_INPT;  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIO_KEY_INPT, &GPIO_InitStructure);
 }
 
 
